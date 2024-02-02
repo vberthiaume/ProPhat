3c7b3d4bbb139e68ed01b8b5cb9e0a8d2b0170d7
Vincent Berthiaume <vberthiaume@gmail.com>
2024-02-02 12:01:00 -0500

    dbg

--- Source/DSP/ProPhatProcessor.cpp
+++ Source/DSP/ProPhatProcessor.cpp
@@ -109,6 +109,9 @@ void ProPhatProcessor::process (juce::AudioBuffer<T>& buffer, juce::MidiBuffer& 
     //we're not dealing with any inputs here, so clear the buffer
     buffer.clear();
 
+    if (midiMessages.getNumEvents () > 0)
+        DBG ('.');
+
     //render the block
     proPhatSynth.renderNextBlock (buffer, midiMessages, 0, buffer.getNumSamples());
 
