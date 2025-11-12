55e867dce56d03cef627cd7f03929d0d0e37ee3a
Vincent Berthiaume <vberthiaume@gmail.com>
2025-11-12 13:26:41 -0500

    move sanitizer tests to their own file

--- tests/PluginBasics.cpp
+++ tests/PluginBasics.cpp
@@ -3,11 +3,6 @@
 #include <catch2/catch_test_macros.hpp>
 #include <catch2/matchers/catch_matchers_string.hpp>
 
-TEST_CASE ("one is equal to one", "[dummy]")
-{
-    REQUIRE (1 == 1);
-}
-
 TEST_CASE ("Plugin instance", "[instance]")
 {
     ProPhatProcessor testPlugin;
@@ -71,42 +66,6 @@ TEST_CASE ("ProcessBlock generates audio from MIDI input", "[RTSan]")
             for (int i = 0; i < buffer.getNumSamples(); ++i)
                 REQUIRE (std::isfinite (buffer.getSample (ch, i)));
     }
-}
-
-TEST_CASE ("use after free", "[ASan]")
-{
-    int* p = new int (1);
-    delete p;
-    *p = 5; // BOOM: write after free
-}
-
-int counter = 0;
-
-void increment()
-{
-    for (int i = 0; i < 1000; ++i)
-        counter++; // unsynchronized write
-}
-
-TEST_CASE ("data race", "[TSan]")
-{
-#include <thread>
-    std::thread t1 (increment);
-    std::thread t2 (increment);
-    t1.join();
-    t2.join();
-}
-
-TEST_CASE ("out-of-bounds write", "[UBSan]")
-{
-    int arr[3];
-    arr[3] = 42; // BOOM: write past end of array
-}
-
-TEST_CASE ("stack buffer overflow", "[UBSan]")
-{
-    char buf[8];
-    buf[8] = 'x'; // BOOM: write past end of stack buffer
 }
 
 #ifdef PAMPLEJUCE_IPP
