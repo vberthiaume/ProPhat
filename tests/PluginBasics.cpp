7c5d4ed64de3d8d462c112411c78bcc0f5cd6749
Vincent Berthiaume <vberthiaume@gmail.com>
2025-11-12 13:19:17 -0500

    cleanup

--- tests/PluginBasics.cpp
+++ tests/PluginBasics.cpp
@@ -19,7 +19,7 @@ TEST_CASE ("Plugin instance", "[instance]")
     }
 }
 
-TEST_CASE ("processBlock generates audio from MIDI input", "[processBlock]")
+TEST_CASE ("ProcessBlock generates audio from MIDI input", "[RTSan]")
 {
     ProPhatProcessor processor;
 
@@ -73,31 +73,11 @@ TEST_CASE ("processBlock generates audio from MIDI input", "[processBlock]")
     }
 }
 
-TEST_CASE ("out-of-bounds write", "[ubsan]")
-{
-    int arr[3];
-    arr[3] = 42; // BOOM: write past end of array
-}
-
-TEST_CASE ("heap buffer overflow", "[asan]")
-{
-    int* arr = new int[3];
-    arr[3]   = 42; // Out-of-bounds heap write
-    delete[] arr;
-}
-
-TEST_CASE ("use after free", "[asan]")
+TEST_CASE ("use after free", "[ASan]")
 {
     int* p = new int (1);
     delete p;
     *p = 5; // BOOM: write after free
-}
-
-// this is really the same thing as the "out-of-bounds write", but just one past the end of the buffer, so potentially not useful
-TEST_CASE ("stack buffer overflow", "[asan]")
-{
-    char buf[8];
-    buf[8] = 'x'; // BOOM: write past end of stack buffer
 }
 
 int counter = 0;
@@ -108,13 +88,25 @@ void increment()
         counter++; // unsynchronized write
 }
 
-TEST_CASE ("data race", "[tsan]")
+TEST_CASE ("data race", "[TSan]")
 {
 #include <thread>
     std::thread t1 (increment);
     std::thread t2 (increment);
     t1.join();
     t2.join();
+}
+
+TEST_CASE ("out-of-bounds write", "[UBSan]")
+{
+    int arr[3];
+    arr[3] = 42; // BOOM: write past end of array
+}
+
+TEST_CASE ("stack buffer overflow", "[UBSan]")
+{
+    char buf[8];
+    buf[8] = 'x'; // BOOM: write past end of stack buffer
 }
 
 #ifdef PAMPLEJUCE_IPP
