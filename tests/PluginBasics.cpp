ed5608021ee33d3c5e9361f25c6be4089372565a
Vincent Berthiaume <vberthiaume@gmail.com>
2025-11-13 12:08:18 -0500

    Add a bunch of sanitizers (#55)
    * rtsan running ok on linux
    
    * attempt to run rtsan on the pipeline
    
    * attempt to fix pipeline, also deleted a bunch of comments which I probably want to keep
    
    * again
    
    * again
    
    * disable juce_recommended_lto_flags when using rtsan
    
    * add missing linker flags?????
    
    * only build standalone with rtsan
    
    * start to add some processBlock tests
    
    * mac configuration files
    
    * debugging working on mac
    
    * commenting out double test
    
    * uncommenting double processing code. My hunch is that this is the mac issue I've encountered before, where some index is just on the verge of being correct but isn't
    
    * firstDraft
    
    * use default clang for new jobs
    
    * tests to trigger asan and tsan
    
    * attempt to cause asan to fail the pipeline tests
    
    * add individual UBSan job. Cleanup the other ones
    
    * trying to fix ubsan configure, really not sure about this
    
    * revert
    
    * more
    
    * cleanup
    
    * move sanitizer tests to their own file
    
    * put back the san tests in the OG file because it won't build in a separate file lol
    
    * comment out sanitizer tests
    
    * comment [skip ci]
    
    * cleanup
    
    * cleanup
    
    * cleaanup, put back some old comments, and re-activate sanitizer sanity tests
    
    * asanoptions
    
    * apparently it has to be ubsan options
    
    * apparently asan just needs all the ubsan stuff
    
    * comment out sanitizer sanity tests

--- tests/PluginBasics.cpp
+++ tests/PluginBasics.cpp
@@ -3,11 +3,6 @@
 #include <catch2/catch_test_macros.hpp>
 #include <catch2/matchers/catch_matchers_string.hpp>
 
-TEST_CASE ("one is equal to one", "[dummy]")
-{
-    REQUIRE (1 == 1);
-}
-
 TEST_CASE ("Plugin instance", "[instance]")
 {
     ProPhatProcessor testPlugin;
@@ -19,6 +14,98 @@ TEST_CASE ("Plugin instance", "[instance]")
     }
 }
 
+TEST_CASE ("ProcessBlock generates audio from MIDI input", "[RTSan]")
+{
+    ProPhatProcessor processor;
+
+    const double sampleRate = 48000.0;
+    const int    blockSize  = 128;
+
+    // --- FLOAT PROCESSING ---
+    SECTION ("float buffer")
+    {
+        juce::AudioBuffer<float> buffer (2, blockSize);
+        juce::MidiBuffer         midi;
+
+        // Add a simple MIDI note-on + note-off pair
+        midi.addEvent (juce::MidiMessage::noteOn (1, 60, (juce::uint8) 100), 0);
+        midi.addEvent (juce::MidiMessage::noteOff (1, 60), blockSize / 2);
+
+        buffer.clear();
+        processor.prepareToPlay (sampleRate, blockSize);
+
+        REQUIRE_FALSE (processor.isUsingDoublePrecision());
+
+        REQUIRE_NOTHROW (processor.processBlock (buffer, midi));
+
+        // The buffer should now contain finite values (could be silence if envelope not yet triggered)
+        for (int ch = 0; ch < buffer.getNumChannels(); ++ch)
+            for (int i = 0; i < buffer.getNumSamples(); ++i)
+                REQUIRE (std::isfinite (buffer.getSample (ch, i)));
+    }
+
+    // --- DOUBLE PROCESSING ---
+    SECTION ("double buffer")
+    {
+        juce::AudioBuffer<double> buffer (2, blockSize);
+        juce::MidiBuffer          midi;
+
+        midi.addEvent (juce::MidiMessage::noteOn (1, 60, (juce::uint8) 100), 0);
+        midi.addEvent (juce::MidiMessage::noteOff (1, 60), blockSize / 2);
+
+        buffer.clear();
+
+        processor.setProcessingPrecision (juce::AudioProcessor::doublePrecision);
+        processor.prepareToPlay (sampleRate, blockSize);
+
+        REQUIRE (processor.isUsingDoublePrecision());
+
+        REQUIRE_NOTHROW (processor.processBlock (buffer, midi));
+
+        for (int ch = 0; ch < buffer.getNumChannels(); ++ch)
+            for (int i = 0; i < buffer.getNumSamples(); ++i)
+                REQUIRE (std::isfinite (buffer.getSample (ch, i)));
+    }
+}
+
+//================================== Sanitizer Sanity Tests ================================================
+#if 0
+TEST_CASE ("use after free", "[ASan]")
+{
+    int* p = new int (1);
+    delete p;
+    *p = 5; // BOOM: write after free
+}
+
+int counter = 0;
+
+void increment()
+{
+    for (int i = 0; i < 1000; ++i)
+        counter++; // unsynchronized write
+}
+
+TEST_CASE ("data race", "[TSan]")
+{
+#include <thread>
+    std::thread t1 (increment);
+    std::thread t2 (increment);
+    t1.join();
+    t2.join();
+}
+
+TEST_CASE ("out-of-bounds write", "[UBSan]")
+{
+    int arr[3];
+    arr[3] = 42; // BOOM: write past end of array
+}
+
+TEST_CASE ("stack buffer overflow", "[UBSan]")
+{
+    char buf[8];
+    buf[8] = 'x'; // BOOM: write past end of stack buffer
+}
+#endif
 
 #ifdef PAMPLEJUCE_IPP
     #include <ipp.h>
