cef83be598a29dcd0752dce67c104e9d1945738a
Vincent Berthiaume <vberthiaume@gmail.com>
2025-12-15 10:47:07 -0500

    update juce

--- source/DSP/LockFreeSynthesiser.cpp
+++ source/DSP/LockFreeSynthesiser.cpp
@@ -22,6 +22,54 @@
 
 #include "LockFreeSynthesiser.h"
 
+LockFreeSynthesiserVoice::LockFreeSynthesiserVoice() {}
+LockFreeSynthesiserVoice::~LockFreeSynthesiserVoice() {}
+
+bool LockFreeSynthesiserVoice::isPlayingChannel (const int midiChannel) const
+{
+    return currentPlayingMidiChannel == midiChannel;
+}
+
+void LockFreeSynthesiserVoice::setCurrentPlaybackSampleRate (const double newRate)
+{
+    currentSampleRate = newRate;
+}
+
+bool LockFreeSynthesiserVoice::isVoiceActive() const
+{
+    return getCurrentlyPlayingNote() >= 0;
+}
+
+void LockFreeSynthesiserVoice::clearCurrentNote()
+{
+    currentlyPlayingNote = -1;
+    currentlyPlayingSound = nullptr;
+    currentPlayingMidiChannel = 0;
+}
+
+void LockFreeSynthesiserVoice::aftertouchChanged (int) {}
+void LockFreeSynthesiserVoice::channelPressureChanged (int) {}
+
+bool LockFreeSynthesiserVoice::wasStartedBefore (const LockFreeSynthesiserVoice& other) const noexcept
+{
+    return noteOnTime < other.noteOnTime;
+}
+
+void LockFreeSynthesiserVoice::renderNextBlock (juce::AudioBuffer<double>& outputBuffer,
+                                        int startSample, int numSamples)
+{
+    juce::AudioBuffer<double> subBuffer (outputBuffer.getArrayOfWritePointers(),
+                                   outputBuffer.getNumChannels(),
+                                   startSample, numSamples);
+
+    tempBuffer.makeCopyOf (subBuffer, true);
+    renderNextBlock (tempBuffer, 0, numSamples);
+    subBuffer.makeCopyOf (tempBuffer, true);
+}
+
+
+//======================================================
+
 LockFreeSynthesiser::LockFreeSynthesiser()
 {
     for (int i = 0; i < juce::numElementsInArray (lastPitchWheelValues); ++i)
